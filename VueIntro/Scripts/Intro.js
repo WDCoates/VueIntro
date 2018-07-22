9edce320b9265e9a90ae1507f031f58069346456
W D Coates <wd_coates@hotmail.com>
2018-07-22 23:31:11 +0100

    5 of 11

--- VueIntro/Scripts/Intro.js
+++ VueIntro/Scripts/Intro.js
@@ -1,6 +1,27 @@
-﻿var vueApp = new Vue ({
-    el: "#vueDiv",
+﻿var vueApp = new Vue({
+    el: '#vueDiv',
     data: {
-        Product: 'Putter'
+        Product: 'Putter',
+        Desc: 'Odyssey EXO Seven Putter',
+        image: '../Images/putters-1.png',
+        clink: 'https://www.callawaygolf.com/clubs/odyssey-putters/exo/putters-2018-exo-seven.html',
+        stock: 100,
+        lots: true,
+        parts: ['Grip Rubber', 'Shaft Steel', 'Head Titanium'],
+        variants: [{ vId: 1, vColour: 'Red', vImage: '../Images/putters-1.png' },
+            { vId: 2, vColour: 'Black', vImage: '../Images/BlackGrip-1-12185.png' }],
+        ordered: 0
+    },
+    methods: {
+        addToCart: function () {
+            vueApp.ordered += 1;
+        },
+        subFromCart: function () {
+            if (this.ordered > 0)
+                this.ordered -= 1;
+        },
+        changeImage (vImage) {
+            this.image = vImage;
+        }
     }
 })
